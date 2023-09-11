1246cc5828732fc5b27f7c9eaabe1cd8e46b8dca
Dypink <76941571+ProjectWindy@users.noreply.github.com>
2023-09-11 09:49:40 +0700

    Add files via upload

---
+++ lib/config/agora_config.dart
@@ -0,0 +1,13 @@
+import 'package:whatsapp_clone/constants.dart';
+
+class AgoraConfig {
+  static String token = '';
+  static String appId = '3a7a04aa00454d57a6b092b38d662155';
+   static String appcertificate = 'd5e6ae315d1441caaac2e40b076037f9';
+  static String baseUrl = AgoraConfig.baseUrl;
+}
+// // class AgoraConfig {
+//   static String token = '';
+//   static String appId = '3a7a04aa00454d57a6b092b38d662155';
+//   static String appcertificate = 'd5e6ae315d1441caaac2e40b076037f9';
+// }
---
+++ lib/constants.dart
@@ -0,0 +1,6 @@
+class AgoraConfig {
+  static const String token = '';
+  static const String appId = '3a7a04aa00454d57a6b092b38d662155';
+  static const String appcertificate = 'd5e6ae315d1441caaac2e40b076037f9';
+  static const String baseUrl = 'https://venerable-sawine-dd6f1c.netlify.app';
+}
